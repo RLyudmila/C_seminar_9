bf184c23f55d2fb41dc4e91d88f8e04cf6fc63e5
RLyudmila <116638126+RLyudmila@users.noreply.github.com>
2022-12-21 15:40:16 +0300

    66

--- 64/Program.cs
+++ 64/Program.cs
@@ -8,7 +8,7 @@
 Console.WriteLine("Введите натуральное число больше 1:");
 int number = int.Parse(Console.ReadLine());
 
-///Метод вывода натуральных чисел от N до 1:
+// Метод вывода натуральных чисел от N до 1:
 void NumberCounter (int number)
 {
     if (number < 0) Console.Write($"{number} не натуральное число");
